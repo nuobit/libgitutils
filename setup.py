b7290d758ff399c5b862f2d56295c46f88835571
Eric Antones <eantones@nuobit.com>
2016-10-07 16:54:48 +0200

    [FIX] pypi production

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='libgitutils',
-      version='0.0.1',
+      version='v0.1.0',
       author="NuoBiT Solutions, S.L., Eric Antones",
       author_email='eantones@nuobit.com',
       package_dir={'libgitutils': 'src'},
